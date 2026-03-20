02856cd593ac6000ef859d5127cb17bd922a7198
Tommy Stanley <tommy.stanley96@gmail.com>
2026-03-20 00:24:05 -0500

    Fix output node rendering

--- nodeplot-gui/src/node_impl/output.cpp
+++ nodeplot-gui/src/node_impl/output.cpp
@@ -4,13 +4,14 @@
 
 template <>
 void node_render(RenderNodeGraph* rng, OutputNode& node) {
-    // TODO: This will crash if grid cols or grid rows comes from pin input
-
     node_generic_render(
         rng,
         node.name().c_str(),
         node,
         [&](auto input) {
+            void* empty = nullptr;
+            input(empty, "dummy", "Graphs", [&](RenderNodeGraph* rng, OutputNode& node, const char* input_id, auto& storage) { return false; });
+
             for (size_t i = 0; i < node.i_graphs.size(); i++) {
                 ImGui::PushID(i);
                 input(node.i_graphs[i], "graph", std::format("Graph ({}, {})", (i % node.i_grid_cols) + 1, (i / node.i_grid_cols) + 1).c_str(), default_input_element);
@@ -21,7 +22,6 @@ void node_render(RenderNodeGraph* rng, OutputNode& node) {
             input(node.i_width, "width", "Width", default_input_element);
             input(node.i_height, "height", "Height", default_input_element);
             input(node.i_grid_cols, "grid_cols", "Grid Cols", [&](RenderNodeGraph* rng, OutputNode& node, const char* input_id, int32_t& input) {
-                ImGui::SetNextItemWidth(200 * rng->scene_scale);
                 if (ImGui::InputInt("##", &input)) {
                     node.i_graphs.resize(node.i_grid_cols * node.i_grid_rows);
                     return true;
@@ -29,7 +29,6 @@ void node_render(RenderNodeGraph* rng, OutputNode& node) {
                 return false;
             });
             input(node.i_grid_rows, "grid_rows", "Grid Rows", [&](RenderNodeGraph* rng, OutputNode& node, const char* input_id, int32_t& input) {
-                ImGui::SetNextItemWidth(200 * rng->scene_scale);
                 if (ImGui::InputInt("##", &input)) {
                     node.i_graphs.resize(node.i_grid_cols * node.i_grid_rows);
                     return true;
