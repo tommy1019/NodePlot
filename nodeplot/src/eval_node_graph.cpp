9fa87de156c77d65964f13172a22a86ecef42a19
Tommy Stanley <tommy.stanley96@gmail.com>
2026-09-10 18:38:03 -0500

    Use default value if node storage is missing entry

--- nodeplot/src/eval_node_graph.cpp
+++ nodeplot/src/eval_node_graph.cpp
@@ -33,14 +33,28 @@ ErrorOr<Data> EvaluatedNodeGraph::get_output_data(NodePlotFile* npf, NodeId node
 
 ErrorOr<Data> EvaluatedNodeGraph::get_input_data(NodePlotFile* npf, NodeId node_id, InputId input_id) {
     auto& ng = TRY(node_graph(npf)).get();
-    NodeGraph::NodeStorage& node_input_storage = TRY(Utils::try_find(ng.nodes, node_id, "Invalid NodeID")).get();
-    auto& input_storage = TRY(Utils::try_find(node_input_storage.input_storage, input_id, "Invalid InputID: " + input_id)).get();
+    NodeGraph::NodeStorage& node_storage = TRY(Utils::try_find(ng.nodes, node_id, "Invalid NodeID")).get();
+    auto input_storage_or_error = Utils::try_find(node_storage.input_storage, input_id, "Invalid InputID: " + input_id);
+
+    if (!input_storage_or_error.has_value()) {
+        auto inputs = TRY(TRY(Utils::try_find(NodeRegistry::node_map, node_storage.type_id, "Invalid Node Type ID")).get().inputs(npf, this, node_id));
+
+        for (auto& input : inputs) {
+            if (input.first == input_id) {
+                if (input.second.default_value.has_value())
+                    return input.second.default_value.value();
+                break;
+            }
+        }
+
+        return ERR("Node has no value for " + input_id + " and no default value");
+    }
 
     return std::visit(Utils::overloaded{
                           [&](Data data) -> ErrorOr<Data> { return data; },
                           [&](NodeGraph::InputPin input_pin) -> ErrorOr<Data> { return this->get_output_data(npf, input_pin.node_id, input_pin.output_id); },
                       },
-                      input_storage);
+                      input_storage_or_error.value().get());
 }
 
 } // namespace NodePlot
